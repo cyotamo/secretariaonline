e6789ba2a663a4f2dc9e8df4d02c91d556f304a6
cyotamo <cyotamo@yahoo.com.br>
2025-08-06 23:37:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function showSection(sectionId, link){
+ function showSection(sectionId, link){
     document.querySelectorAll('.service-section').forEach(sec => sec.classList.remove('active'));
     document.getElementById(sectionId).classList.add('active');
     document.querySelectorAll('nav a').forEach(a => a.classList.remove('active'));
