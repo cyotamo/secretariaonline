dd251c65b3cf3036dde5610d189dffc5923258ee
cyotamo <cyotamo@yahoo.com.br>
2025-08-06 22:57:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSection(sectionId, link){
+    document.querySelectorAll('.service-section').forEach(sec => sec.classList.remove('active'));
+    document.getElementById(sectionId).classList.add('active');
+    document.querySelectorAll('nav a').forEach(a => a.classList.remove('active'));
+    link.classList.add('active');
+    toggleMenu(); // Fecha o menu no mobile
+}
+function toggleMenu(){
+    document.querySelector('.menu-links').classList.toggle('show');
+}
